6201bcaf06fe342741e896859898cd2ffabdda09
Edgar Azevedo <edgarvazevedo@gmail.com>
2023-05-21 21:37:03 +0100

    corrigindo erros do submit

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -10,73 +10,101 @@ export default class ListaTarefas extends React.Component {
     // Inicializa o estado do componente com uma lista de tarefas vazia e uma nova tarefa vazia
     this.state = {
       tarefas: [],
-      novaTarefa: "",
+      novaTarefa: {
+        descricao: "",
+        prioridade: "",
+      },
     };
   }
 
   handleChange = (event) => {
-    // Atualiza o estado com o valor digitado no campo de texto da nova tarefa
-    this.setState({ novaTarefa: event.target.value });
+    this.setState({
+      novaTarefa: {
+        ...this.state.novaTarefa,
+        descricao: event.target.value,
+      },
+    });
+  };
+
+  handleChangePrioridade = (event) => {
+    this.setState({
+      novaTarefa: {
+        ...this.state.novaTarefa,
+        prioridade: event.target.value,
+      },
+    });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     const { tarefas, novaTarefa } = this.state;
-    // Verifica se a nova tarefa não está vazia antes de adicioná-la à lista de tarefas
-    if (novaTarefa.trim() === "") return;
+    if (
+      novaTarefa.descricao.trim() === "" ||
+      novaTarefa.prioridade.trim() === ""
+    )
+      return;
 
-    // Cria uma cópia da lista de tarefas atual e adiciona a nova tarefa à cópia
-    const updateTarefa = [...tarefas, novaTarefa];
-    // Atualiza o estado com a lista de tarefas atualizada e limpa o campo da nova tarefa
-    this.setState({ tarefas: updateTarefa, novaTarefa: "" });
+    const updatedTarefas = [...tarefas, novaTarefa];
+    this.setState({
+      tarefas: updatedTarefas,
+      novaTarefa: {
+        descricao: "",
+        prioridade: "",
+      },
+    });
   };
 
   handleDelete = (index) => {
     const { tarefas } = this.state;
-    // Remove a tarefa da lista com base no índice fornecido
-    const updateTarefa = tarefas.filter((tarefas, i) => i !== index);
-    // Atualiza o estado com a lista de tarefas atualizada (sem a tarefa removida)
-    this.setState({ tarefas: updateTarefa });
+    const updatedTarefas = tarefas.filter((_, i) => i !== index);
+    this.setState({ tarefas: updatedTarefas });
   };
 
   render() {
     const { tarefas, novaTarefa } = this.state;
     return (
-      <div className="">
+      <div className="container">
         <Form onSubmit={this.handleSubmit}>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>State</Form.Label>
-              <Form.Select defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
-              </Form.Select>
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Group as={Col} md={6} controlId="textarea">
               <Form.Label>Tarefa</Form.Label>
+              <Form.Select
+                defaultValue="Prioridade"
+                value={novaTarefa.prioridade}
+                onChange={this.handleChangePrioridade}
+              >
+                <option>Baixa</option>
+                <option>Média</option>
+                <option>Alta</option>
+              </Form.Select>
               <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={novaTarefa}
+                type="text"
+                placeholder="Tarefa"
+                value={novaTarefa.descricao}
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <input
-              type="text"
-              value={novaTarefa}
-              onChange={this.handleChange}
-            />
             <Button type="submit">Adicionar</Button>
           </Row>
         </Form>
-        <ul>
-          {tarefas.map((tarefas, index) => (
-            <li key={index}>
-              {tarefas}
-              <Button onClick={() => this.handleDelete}>Excluir</Button>
-            </li>
-          ))}
-        </ul>
+        <Row className="mb-3">
+          <Col>
+            <h5>Tarefa</h5>
+            <ul>
+              {tarefas.map((tarefa, index) => (
+                <li key={index}>
+                  {tarefa.descricao} - {tarefa.prioridade}
+                  <Button
+                    variant="danger"
+                    onClick={() => this.handleDelete(index)}
+                  >
+                    Excluir
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
       </div>
     );
   }
